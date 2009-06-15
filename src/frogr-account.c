3f2bfcd16fcb4b6024bd8e565dcaae3c513816ce
Adrian Perez <aperez@igalia.com>
2009-06-15 18:56:27 +0200

    FrogrAccount to save Flickr account information

---
+++ src/frogr-account.c
@@ -0,0 +1,371 @@
+/*
+ * frogr-config.c -- Configuration system for Frogr.
+ *
+ * Copyright (C) 2009 Adrian Perez
+ * Authors: Adrian Perez <aperez@igalia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "frogr-account.h"
+#include <libxml/parser.h>
+#include <errno.h>
+
+
+#define FROGR_ACCOUNT_GET_PRIVATE(object) \
+  (G_TYPE_INSTANCE_GET_PRIVATE ((object), FROGR_ACCOUNT_TYPE, FrogrAccountPrivate))
+
+G_DEFINE_TYPE (FrogrAccount, frogr_account, G_TYPE_OBJECT);
+
+
+/* Private structure */
+typedef struct _FrogrAccountPrivate FrogrAccountPrivate;
+struct _FrogrAccountPrivate
+{
+  gchar *frob;
+  gchar *token;
+  gchar *username;
+
+  gboolean enabled;
+  gboolean public;
+  gboolean family;
+  gboolean friends;
+};
+
+
+/* Properties */
+enum
+{
+  PROP_0,
+  PROP_FROB,
+  PROP_TOKEN,
+  PROP_USERNAME,
+  PROP_ENABLED,
+  PROP_PUBLIC,
+  PROP_FAMILY,
+  PROP_FRIENDS,
+};
+
+
+
+/* Private API bits */
+
+static void
+_frogr_account_set_property (GObject      *object,
+                             guint         property_id,
+                             const GValue *value,
+                             GParamSpec   *pspec)
+{
+  FrogrAccountPrivate *priv = FROGR_ACCOUNT_GET_PRIVATE (object);
+
+  switch (property_id) {
+    case PROP_FROB:
+      g_free (priv -> frob);
+      priv -> frob = g_value_dup_string (value);
+      break;
+    case PROP_TOKEN:
+      g_free (priv -> token);
+      priv -> token = g_value_dup_string (value);
+      break;
+    case PROP_USERNAME:
+      g_free (priv -> username);
+      priv -> username = g_value_dup_string (value);
+      break;
+    case PROP_ENABLED:
+      priv -> enabled = g_value_get_boolean (value);
+      break;
+    case PROP_PUBLIC:
+      priv -> public = g_value_get_boolean (value);
+      break;
+    case PROP_FAMILY:
+      priv -> family = g_value_get_boolean (value);
+      break;
+    case PROP_FRIENDS:
+      priv -> friends = g_value_get_boolean (value);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+
+
+static void
+_frogr_account_get_property (GObject    *object,
+                             guint       property_id,
+                             GValue     *value,
+                             GParamSpec *pspec)
+{
+  FrogrAccountPrivate *priv = FROGR_ACCOUNT_GET_PRIVATE (object);
+
+  switch (property_id) {
+    case PROP_FROB:
+      g_value_set_string (value, priv -> frob);
+      break;
+    case PROP_TOKEN:
+      g_value_set_string (value, priv -> token);
+      break;
+    case PROP_USERNAME:
+      g_value_set_string (value, priv -> username);
+      break;
+    case PROP_ENABLED:
+      g_value_set_boolean (value, priv -> enabled);
+      break;
+    case PROP_PUBLIC:
+      g_value_set_boolean (value, priv -> public);
+      break;
+    case PROP_FAMILY:
+      g_value_set_boolean (value, priv -> family);
+      break;
+    case PROP_FRIENDS:
+      g_value_set_boolean (value, priv -> friends);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+
+static void
+_frogr_account_finalize (GObject *object)
+{
+  FrogrAccountPrivate *priv = FROGR_ACCOUNT_GET_PRIVATE (object);
+
+  g_free (priv -> frob);
+  g_free (priv -> token);
+  g_free (priv -> username);
+
+  /* Call superclass */
+  G_OBJECT_CLASS (frogr_account_parent_class) -> finalize (object);
+}
+
+
+
+
+static void
+frogr_account_class_init (FrogrAccountClass *klass)
+{
+  GObjectClass *obj_class = G_OBJECT_CLASS (klass);
+  GParamSpec *pspec;
+
+  g_type_class_add_private (klass, sizeof (FrogrAccountPrivate));
+
+  obj_class -> get_property = _frogr_account_get_property;
+  obj_class -> set_property = _frogr_account_set_property;
+  obj_class -> finalize     = _frogr_account_finalize;
+
+  pspec = g_param_spec_string ("frob",
+                               "Flickr API authentication frob",
+                               "Get/set Flickr frob",
+                               "",
+                               G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_FROB, pspec);
+
+  pspec = g_param_spec_string ("token",
+                               "Flickr API authentication token",
+                               "Get/set Flickr token",
+                               "",
+                               G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_TOKEN, pspec);
+
+  pspec = g_param_spec_string ("username",
+                               "Flickr account user name",
+                               "Get/set Flickr user name",
+                               "",
+                               G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_USERNAME, pspec);
+
+  pspec = g_param_spec_boolean ("enabled",
+                                "Whether the account is enabled",
+                                "Get/set enabled status",
+                                TRUE,
+                                G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_ENABLED, pspec);
+
+  pspec = g_param_spec_boolean ("public",
+                                "Whether photos are public by default",
+                                "Get/set private by default",
+                                FALSE,
+                                G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_PUBLIC, pspec);
+
+  pspec = g_param_spec_boolean ("family",
+                                "If sharing is private, whether to share with family",
+                                "Get/set family sharing",
+                                FALSE,
+                                G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_FAMILY, pspec);
+
+  pspec = g_param_spec_boolean ("friends",
+                                "If sharing is private, whether to share with friends",
+                                "Get/set family sharing",
+                                FALSE,
+                                G_PARAM_READWRITE);
+  g_object_class_install_property (obj_class, PROP_FRIENDS, pspec);
+}
+
+
+static void
+frogr_account_init (FrogrAccount *faccount)
+{
+  FrogrAccountPrivate *priv = FROGR_ACCOUNT_GET_PRIVATE (faccount);
+
+  priv -> frob = NULL;
+  priv -> token = NULL;
+  priv -> username = NULL;
+  priv -> enabled = TRUE;
+  priv -> public = FALSE;
+  priv -> family = FALSE;
+  priv -> friends = FALSE;
+}
+
+
+gchar*
+frogr_account_get_frob (FrogrAccount *faccount)
+{
+  gchar *value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), NULL);
+  g_object_get (faccount, "frob", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_frob (FrogrAccount *faccount,
+                               const gchar        *value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_return_if_fail (value != NULL);
+  g_object_set (faccount, "frob", value, NULL);
+}
+
+
+gchar*
+frogr_account_get_token (FrogrAccount *faccount)
+{
+  gchar *value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), NULL);
+  g_object_get (faccount, "token", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_token (FrogrAccount *faccount,
+                                const gchar        *value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_return_if_fail (value != NULL);
+  g_object_set (faccount, "token", value, NULL);
+}
+
+
+gchar*
+frogr_account_get_username (FrogrAccount *faccount)
+{
+  gchar *value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), NULL);
+  g_object_get (faccount, "username", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_username (FrogrAccount *faccount,
+                                   const gchar        *value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_return_if_fail (value != NULL);
+  g_object_set (faccount, "username", value, NULL);
+}
+
+
+gboolean
+frogr_account_get_enabled (FrogrAccount *faccount)
+{
+  gboolean value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), FALSE);
+  g_object_get (faccount, "enabled", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_enabled (FrogrAccount *faccount,
+                                  gboolean            value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_object_set (faccount, "enabled", value, NULL);
+}
+
+
+gboolean
+frogr_account_get_public (FrogrAccount *faccount)
+{
+  gboolean value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), FALSE);
+  g_object_get (faccount, "public", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_public (FrogrAccount *faccount,
+                                 gboolean            value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_object_set (faccount, "public", value, NULL);
+}
+
+
+gboolean
+frogr_account_get_family (FrogrAccount *faccount)
+{
+  gboolean value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), FALSE);
+  g_object_get (faccount, "family", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_family (FrogrAccount *faccount,
+                                 gboolean            value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_object_set (faccount, "family", value, NULL);
+}
+
+
+gboolean
+frogr_account_get_friends (FrogrAccount *faccount)
+{
+  gboolean value;
+  g_return_val_if_fail (FROGR_IS_ACCOUNT (faccount), FALSE);
+  g_object_get (faccount, "friends", &value, NULL);
+  return value;
+}
+
+
+void
+frogr_account_set_friends (FrogrAccount *faccount,
+                                  gboolean            value)
+{
+  g_return_if_fail (FROGR_IS_ACCOUNT (faccount));
+  g_object_set (faccount, "friends", value, NULL);
+}
+
---
+++ src/frogr-account.h
@@ -0,0 +1,88 @@
+/*
+ * frogr-config.h -- Account class for Frogr.
+ *
+ * Copyright (C) 2009 Adrian Perez
+ * Authors: Adrian Perez <aperez@igalia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _FROGR_ACCOUNT_H
+#define _FROGR_ACCOUNT_H
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define FROGR_ACCOUNT_TYPE            (frogr_account_get_type())
+#define FROGR_IS_ACCOUNT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE(obj, FROGR_ACCOUNT_TYPE))
+#define FROGR_IS_ACCOUNT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE(klass,  FROGR_ACCOUNT_TYPE))
+#define FROGR_ACCOUNT(obj)            (G_TYPE_CHECK_INSTANCE_CAST(obj, FROGR_ACCOUNT_TYPE, FrogrAccount))
+#define FROGR_ACCOUNT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST(klass,  FROGR_ACCOUNT_TYPE, FrogrAccountClass))
+#define FROGR_ACCOUNT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS(obj,  FROGR_ACCOUNT_TYPE, FrogrAccountClass))
+
+typedef struct _FrogrAccount      FrogrAccount;
+typedef struct _FrogrAccountClass FrogrAccountClass;
+
+
+struct _FrogrAccount
+{
+	GObject parent_instance;
+};
+
+
+struct _FrogrAccountClass
+{
+	GObjectClass parent_class;
+};
+
+
+GType frogr_account_get_type(void) G_GNUC_CONST;
+
+FrogrAccount* frogr_account_new (const gchar *first_property, ...);
+
+gchar* frogr_account_get_frob (FrogrAccount *faccount);
+void   frogr_account_set_frob (FrogrAccount *faccount,
+                               const gchar *frob);
+
+gchar* frogr_account_get_token (FrogrAccount *faccount);
+void   frogr_account_set_token (FrogrAccount *faccount,
+                                const gchar *token);
+
+gchar* frogr_account_get_username (FrogrAccount *faccount);
+void   frogr_account_set_username (FrogrAccount *faccount,
+                                   const gchar *username);
+
+gboolean frogr_account_get_enabled (FrogrAccount *faccount);
+void     frogr_account_set_enabled (FrogrAccount *faccount,
+                                    gboolean value);
+
+gboolean frogr_account_get_public (FrogrAccount *faccount);
+void     frogr_account_set_public (FrogrAccount *faccount,
+                                   gboolean value);
+
+gboolean frogr_account_get_private_family (FrogrAccount *faccount);
+void     frogr_account_set_private_family (FrogrAccount *faccount,
+                                           gboolean value);
+
+gboolean frogr_account_get_private_friends (FrogrAccount *faccount);
+void     frogr_account_set_private_friends (FrogrAccount *faccount,
+                                            gboolean value);
+
+G_END_DECLS
+
+#endif /* !_FROGR_ACCOUNT_H */
+
