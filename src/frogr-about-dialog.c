e2a34e57de4bc720c825cba24c486cd73b9ef978
Mario Sanchez Prada <msanchez@igalia.com>
2011-02-05 13:25:57 +0100

    Frogr 0.4 released

--- src/frogr-about-dialog.c
+++ src/frogr-about-dialog.c
@@ -90,8 +90,7 @@ frogr_about_dialog_show (GtkWindow *parent)
                                    NULL);
 #endif
 
-  /* XXX: Show clearly that this is an unreleased version */
-  version = g_strdup_printf ("%s~unreleased", VERSION);
+  version = g_strdup_printf ("%s", VERSION);
 
   /* Show about dialog */
   gtk_show_about_dialog (GTK_WINDOW (parent),
